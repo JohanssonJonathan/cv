dab274de2e18511599741bc935d1fd5fa746bc42
JohanssonJonathan <jjohanssonjonathan@gmail.com>
2019-03-24 19:17:58 +0100

    styling

--- src/Cv.js
+++ src/Cv.js
@@ -3,7 +3,7 @@ import "./css/App.css";
 import "./css/cv.css";
 import Transition from "./UI/Transition";
 import Fade from "react-reveal/Fade";
-import profil from "./images/profil.png";
+import profil from "./images/jonte.jpg";
 import html from "./images/skills/html5.png";
 import css from "./images/skills/css3.png";
 import es from "./images/skills/es6.png";
@@ -15,12 +15,14 @@ import github from "./images/skills/github.png";
 import mongodb from "./images/skills/mongodb.png";
 import nodejs from "./images/skills/nodejs.png";
 import vue from "./images/skills/vue.png";
+import kid from "./images/profil.png";
+
 
 const localStyles = {
   progressbar: {
     width: "100%",
     position: "absolute",
-    top: 20
+    top: 0
   },
   text: {
     textAlign: "left"
@@ -114,9 +116,11 @@ const localStyles = {
   },
   letterContainer: {
     width: "100%",
-    marginTop: 100,
+    height: 450,
     display: "flex",
-    justifyContent: "center"
+    justifyContent: "flex-end",
+    alignItems: "center",
+    borderBottom:"10px solid rgba(0,0,0,.7)"
   },
   letter: { textAlign: "center", width: "50%" }
 };
@@ -129,42 +133,10 @@ const ProgressBar = ({ style, id }) => {
   );
 };
 
-const ProgressBars = ({ style }) => {
-  return (
-    <div style={{ ...style }}>
-      <ProgressBar id="progressbar1" style={{ width: "40%" }} />
-      <ProgressBar id="progressbar2" style={{ width: "27%" }} />
-      <ProgressBar id="progressbar3" style={{ width: "35%" }} />
-      <ProgressBar id="progressbar4" style={{ width: "33%" }} />
-      <ProgressBar id="progressbar5" style={{ width: "40%" }} />
-      <ProgressBar id="progressbar6" style={{ width: "33%" }} />
-      <ProgressBar id="progressbar7" style={{ width: "50%" }} />
-      <ProgressBar id="progressbar8" style={{ width: "30%" }} />
-    </div>
-  );
-};
-
 const Name = () => {
   return (
     <div>
-      <span className="name">Jonathan Johansson</span>
       <div className="underline" />
-    </div>
-  );
-};
-
-const Image = () => {
-  return (
-    <div className="profileFixed">
-      <img src={profil} className="image" />
-    </div>
-  );
-};
-
-const ImageWrapper = ({ children }) => {
-  return (
-    <div className="imageWrapper">
-      <div className="profile">{children}</div>
     </div>
   );
 };
@@ -173,19 +145,17 @@ const Letter = () => {
   return (
     <Fade>
       <div style={localStyles.letterContainer}>
-        <p style={localStyles.letter}>
-          Glad och social frontend-utvecklarstudent på 25 år med ett stort
-          intresse för IT och att bygga webbapplikationer. I grund och botten en
-          väldigt världsvan person som har lätt för att kontakt med folk och
-          lära känna nya människor. Även folk från andra kulturer. Har lätt för
-          skratt och gillar att skämta till det. Även väldigt seriös och
-          allvarlig när det väl gäller. Kombinationen av kod och design är något
-          som jag tycker är väldigt kul. Jag skulle anse mig själv som en person
-          med hög arbetsmoral som alltid strävar efter att göra rätt för sig som
-          samt aldrig hittar luckor för att ta det lugnt. Jag inte bara tror
-          utan jag vet också att jag har möjligheter att kunna utveckla mig
-          själv och erat företag till det bättre. Både i team, design och kod!
-        </p>
+        <div style={{ width: "100%" }}>
+          <span className="font">Hello!</span>
+          <p style={{ marginTop: 50 }}>
+            My name is Jonathan Johansson, and I am a frondend-developer based
+            in Gothenburg Sweden
+          </p>
+          <p>Please scroll down to follow my journey as a developer</p>
+        </div>
+        <div>
+          <img src={kid} className="kid" />
+        </div>
       </div>
     </Fade>
   );
@@ -199,37 +169,10 @@ const Skill = ({ children, style }) => {
   );
 };
 
-const Skills = () => {
-  return (
-    <div style={localStyles.skillsContainer}>
-      <div style={{ position: "relative" }}>
-        <Skill style={localStyles.htmlSkill}>html</Skill>
-        <Skill style={localStyles.cssSkill}>css</Skill>
-        <Skill style={localStyles.javascripSkill}>javascript</Skill>
-        <Skill style={localStyles.nodeSkill}>node</Skill>
-        <Skill style={localStyles.apolloSkill}>apollo</Skill>
-        <Skill style={localStyles.graphqlSkill}>graphql</Skill>
-        <Skill style={localStyles.reactSkill}>react</Skill>
-        <Skill style={localStyles.nativeSkill}>native</Skill>
-        <Skill style={localStyles.vueSkill}>vue</Skill>
-        <Skill style={localStyles.githubSkill}>github</Skill>
-        <Skill style={localStyles.prismaSkill}>prisma</Skill>
-        <Skill style={localStyles.jsonSkill}>json</Skill>
-        <Skill style={localStyles.ajaxSkill}>ajax</Skill>
-        <Skill style={localStyles.mongodbSkill}>mongodb</Skill>
-        <Skill style={localStyles.firebaseSkill}>firebase</Skill>
-      </div>
-    </div>
-  );
-};
-
 const Header = ({ display }) => {
   return (
     <div style={{ display }}>
       <Name />
-      <ImageWrapper>
-        <Image />
-      </ImageWrapper>
     </div>
   );
 };
@@ -238,20 +181,10 @@ const ContentWrapper = ({ children, display }) => {
   return (
     <div
       style={{
-        display,
-        ...localStyles.progressbar
+        display
       }}
     >
       {children}
-    </div>
-  );
-};
-
-const UpperContent = () => {
-  return (
-    <div style={{ position: "relative", width: "100%" }}>
-      <Skills />
-      <ProgressBars style={localStyles.flip} />
     </div>
   );
 };
@@ -281,75 +214,108 @@ const SmallImage = ({ showSmallImage }) => {
   );
 };
 
-const SkillImageContainer = ({showSkills}) => {
+const SectionWrapper = ({ children }) => {
   return (
-    <div className="skillsContent">
-      <SkillIcon
-        src={html}
-        style={{ top: 100, left: "40%" }}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={css}
-        style={{ top: 40, left: "60%" }}
-        delay={50}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={es}
-        style={{ top: 0, left: "30%" }}
-        delay={100}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={graphql}
-        style={{ top: 180, left: "55%" }}
-        delay={150}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={prisma}
-        style={{ top: 150, left: "0%" }}
-        delay={200}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={firebase}
-        style={{ top: 10, left: "10%" }}
-        delay={250}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={react}
-        style={{ top: 100, left: "20%" }}
-        delay={300}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={github}
-        style={{ top: 210, left: "21%" }}
-        delay={350}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={mongodb}
-        style={{ top: 260, left: "50%" }}
-        delay={400}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={nodejs}
-        style={{ top: 300, left: "5%" }}
-        delay={450}
-        showSkills={showSkills}
-      />
-      <SkillIcon
-        src={vue}
-        style={{ top: 300, left: "35%" }}
-        delay={500}
-        showSkills={showSkills}
-      />
+    <div style={{ width: "100%", display: "flex", height: 400, marginBottom:100, marginTop:60, position:"relative" }}>
+      {children}
     </div>
+  );
+};
+
+const SkillImageContainer = ({ showSkills }) => {
+  return (
+    <Fragment>
+
+      <div style={{position:"absolute", height:100, width:"100%", top:100}} className="hej">
+
+      </div>
+      <div className="skillsContent">
+        <SkillIcon
+          src={html}
+          style={{ top: 100, left: "40%" }}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={css}
+          style={{ top: 40, left: "60%" }}
+          delay={50}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={es}
+          style={{ top: 0, left: "30%" }}
+          delay={100}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={graphql}
+          style={{ top: 180, left: "55%" }}
+          delay={150}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={prisma}
+          style={{ top: 150, left: "0%" }}
+          delay={200}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={firebase}
+          style={{ top: 10, left: "10%" }}
+          delay={250}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={react}
+          style={{ top: 100, left: "20%" }}
+          delay={300}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={github}
+          style={{ top: 210, left: "21%" }}
+          delay={350}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={mongodb}
+          style={{ top: 260, left: "50%" }}
+          delay={400}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={nodejs}
+          style={{ top: 300, left: "5%" }}
+          delay={450}
+          showSkills={showSkills}
+        />
+        <SkillIcon
+          src={vue}
+          style={{ top: 300, left: "35%" }}
+          delay={500}
+          showSkills={showSkills}
+        />
+      </div>
+      <Fade when={showSkills} bottom>
+        <div
+          style={{
+            width: "50%",
+            display: "flex",
+            flexDirection: "column",
+            alignSelf: "center",
+            margin: 0
+          }}
+        >
+          <span className="skillHead">Technologies</span>
+          <p className="test">
+            These are the technologies that I know at the moment.{" "}
+          </p>
+          <p className="presstext">
+            Press on each icon to find out more about them
+          </p>
+        </div>
+      </Fade>
+    </Fragment>
   );
 };
 
@@ -368,7 +334,7 @@ class Cv extends Component {
         document.getElementsByTagName("html")[0].scrollTop;
 
       this.setState(state => ({
-        showSmallImage: scrollPos > 260 ? true : false
+        showSmallImage: scrollPos > 400 ? true : false
       }));
 
       if (scrollPos > 300) {
@@ -399,13 +365,48 @@ class Cv extends Component {
             <Header display={this.state.displayOrNot} />
             <ContentWrapper display={this.state.displayOrNot}>
               <SmallImage showSmallImage={showSmallImage} />
-              <UpperContent />
               <div className="content">
                 <Letter />
-                <SkillImageContainer showSkills={showSkills}/>
+                <SectionWrapper>
+                <SkillImageContainer showSkills={showSkills} />
+
+                </SectionWrapper>
+
+                <Fade bottom>
+
+                <SectionWrapper>
+                    <div style={{width:"100%", margin:0}}>
+                      <span className="projecttext">
+                        Projects
+                      </span>
+                      <p>Check out the projects I've made.</p>
+                        <p>You can also check out my github for more indepth investigation</p>
+                      <div style={{backgroundColor:"rgba(0,0,0,0.8)", position:"relative", display:"flex", justifyContent:"space-around", alignItems:"center"}}>
+                          <span className="projects" >Nymbani Skateboards</span>
+                          <span className="projects" >Album list</span>
+
+                          <span className="projects" >Newsify</span>
+                          <span className="projects">Calculator</span>
+                          <span className="projects" >Pace</span>
+                          <span className="projects" >Time for quiz</span>
+
+                      </div>
+                      {/* <div style={{backgroundColor:"white", height:100}}> 
+                        <p>I've always loved skateboarding and other sports that inlucedes boards.</p>
+                        <p>Me and my brother made our own company to make our own boards.</p>
+
+                      </div> */}
+                    
+                    </div>
+                </SectionWrapper>
+
+                </Fade>
+
+                <div />
               </div>
             </ContentWrapper>
           </Wrapper>
+        
         </Transition>
       </React.Fragment>
     );
